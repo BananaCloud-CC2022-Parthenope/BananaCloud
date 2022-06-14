23c045ecf65f8dd5dd4cbca44fb9ff95c7729ebd
Vincenzo Bevilacqua <48207038+ViBevilacqua@users.noreply.github.com>
2022-06-14 16:23:20 +0200

    Resources of the OpenFaaS function

---
+++ OpenFaaS_function/template/pydatascience/function/handler.py
@@ -0,0 +1,19 @@
+import json
+import os
+
+# from .core import utils
+
+
+function_root = os.environ.get("function_root")
+
+# Now  pre-load the model, e.g.
+# from .core import model
+
+
+def handle(req: bytes) -> str:
+    """handle a request to the function
+    Args:
+        req (bytes): request body
+    """
+
+    return json.dumps({"echo": req)
